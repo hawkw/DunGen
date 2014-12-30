490caf09ae23681f30f57255fca3fd2022faad05
Hawk Weisman <hawk@meteorcodelabs.com>
2014-12-30 16:40:17 -0500

    Add some comments

--- src/dungen/Generator.scala
+++ src/dungen/Generator.scala
@@ -4,21 +4,31 @@ package dungen
  * Created by hawk on 12/30/14.
  */
 class Generator(
-                 val maxTries: Int = 20, // max number of room tries
+                 val maxRooms: Int = 20, // max number of room tries
                  val extraSize: Int = 0   // increase this for larger rooms
                  ) {
   val rand = new java.util.Random
 
-  def generate(x: Int, y: Int): Array[Boolean] = {
-    var grid = Array.ofDim[Int](x,y) // start with an array of zeros
-    val rooms = makeRooms(grid)
-    ???
+  /**
+   * Generate a 2D array of booleans of the specified size. True = empty, false = wall.
+   * @param x the max x-dimension for the dungeon grid
+   * @param y the max y-dimension for the dungeon grid
+   * @return a 2D array of booleans of size (x,y)
+   */
+  def generate(x: Int, y: Int): Array[Array[Boolean]] = {
+    val rooms = makeRooms(Array.ofDim[Int](x,y))
+    rooms.map(slice => slice.map(i => if (i > 0) true else false ))
   }
 
-  def makeRooms(grid: Array[Array[Int]]): Array[Array[Int]] = {
-    for {
-      i <- 1 to maxTries
-    } {
+  /**
+   * Carve out rooms on a grid. Each room is colored uniquely.
+   *
+   * Rooms are colored uniquely 'cause I could implement map coloring here but it would make the algorithm much more complex, and just seems unnecessary.
+   * @param grid
+   * @return
+   */
+  protected def makeRooms(grid: Array[Array[Int]]): Array[Array[Int]] = {
+    for { i <- 1 to maxRooms } {
       val size = (rand.nextInt(2 + extraSize) + 1) * 2 + 1
       val rect = rand.nextInt(1 + size / 2) * 2
       val (height: Int,width: Int) = rand nextInt 2 match {
@@ -26,20 +36,14 @@ class Generator(
         case 1 => (size + rect, size)
         case 2 => (size, size + rect)
       }
-
       val ypos = rand.nextInt(grid.length - height) //* 2 + 1
       val xpos = rand.nextInt(grid(0).length - width)// * 2 + 1
       for {
         y <- ypos to ypos + height
         x <- xpos to xpos + width
+        if grid(x)(y) == 0
       } {
-        if (grid(x)(y) == 0) {
-          println(s"Setting ($x,$y)")
-          grid(x)(y) = i
-        } else {
-          println(s"($x,$y) = ${grid(x)(y)}")
-        }
-
+        grid(x)(y) = i
       }
     }
     grid
--- src/dungen/package.scala
+++ src/dungen/package.scala
@@ -8,4 +8,15 @@ package object dungen {
   } {
     print(s"${grid(y)(x)}\t${if (x == grid(y).length-1){"\n"}else{""}}")
   }
+  def prettyPrint(grid: Array[Array[Boolean]]) = for {
+    y <- 0 to grid.length -1
+    x <- 0 to grid(y).length -1
+  } {
+    print(
+      (grid(x)(y) match {
+        case true => "."
+        case false => "#"
+      }) + (if (x == grid(y).length-1) "\n" else "")
+    )
+  }
 }
