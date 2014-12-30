9262c1bdd7535b3cbf1318c549706941546f8dd6
Hawk Weisman <hawk@meteorcodelabs.com>
2014-12-30 17:05:22 -0500

    Started on maze-cutting, not done yet

--- src/dungen/Generator.scala
+++ src/dungen/Generator.scala
@@ -1,7 +1,12 @@
 package dungen
 
+import scala.annotation.tailrec
+
 /**
- * Created by hawk on 12/30/14.
+ * A quick dungeon-grid generator.
+ * Loosely based on the algorithm described in this blog post: [[http://journal.stuffwithstuff.com/2014/12/21/rooms-and-mazes/]]
+ *
+ * @author Hawk Weisman
  */
 class Generator(
                  val maxRooms: Int = 20, // max number of room tries
@@ -16,8 +21,23 @@ class Generator(
    * @return a 2D array of booleans of size (x,y)
    */
   def generate(x: Int, y: Int): Array[Array[Boolean]] = {
-    val rooms = makeRooms(Array.ofDim[Int](x,y))
-    rooms.map(slice => slice.map(i => if (i > 0) true else false ))
+    var rooms = makeRooms(Array.ofDim[Int](x,y))
+    rooms = makeMazes(rooms)
+    rooms.map(slice => slice.map(i => if (i != 0) true else false ))
+  }
+
+  def makeMazes(grid: Array[Array[Int]]): Array[Array[Int]] = {
+    floodfill(0,0)
+    @tailrec def floodfill(x: Int, y: Int): Unit = grid(x)(y) match {
+      case i if i > 0 || i == -1 => {}//already filled
+      case 0 =>
+        grid(x)(y) = -1
+        rand.nextInt(2) match {
+          case 0 => floodfill(if(x + 1 < grid(0).length - 1) x + 1 else x, y)
+          case 1 => floodfill(x, if(y + 1 < grid.length - 1) y + 1 else y)
+        }
+    }
+    grid
   }
 
   /**
@@ -31,13 +51,13 @@ class Generator(
     for { i <- 1 to maxRooms } {
       val size = (rand.nextInt(2 + extraSize) + 1) * 2 + 1
       val rect = rand.nextInt(1 + size / 2) * 2
-      val (height: Int,width: Int) = rand nextInt 2 match {
+      val (height: Int,width: Int) = rand nextInt 3 match {
         case 0 => (size,size) // square room
         case 1 => (size + rect, size)
         case 2 => (size, size + rect)
       }
-      val ypos = rand.nextInt(grid.length - height) //* 2 + 1
-      val xpos = rand.nextInt(grid(0).length - width)// * 2 + 1
+      val ypos = rand.nextInt(grid.length - height -1) //* 2 + 1
+      val xpos = rand.nextInt(grid(0).length - width - 1)// * 2 + 1
       for {
         y <- ypos to ypos + height
         x <- xpos to xpos + width
@@ -48,4 +68,10 @@ class Generator(
     }
     grid
   }
+
+  /**
+   * Crappy main method for debugging
+   * @param argv
+   */
+  def main(argv: Array[String]) = "\n" + prettyPrint(generate(50,50))
 }
